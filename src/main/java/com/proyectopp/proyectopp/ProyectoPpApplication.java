a0db548b2de34739fd68f694e9f3aef7d5768cdc
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-03-09 21:51:19 -0600

    Creacion de la base del proyecto en Spring Boot

---
+++ src/main/java/com/proyectopp/proyectopp/ProyectoPpApplication.java
@@ -0,0 +1,13 @@
+package com.proyectopp.proyectopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoPpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyectopp/proyectopp/ProyectoPpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectopp.proyectopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
